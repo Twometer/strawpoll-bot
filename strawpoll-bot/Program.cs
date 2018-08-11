10a0a50fef2279c8e85f1385fba764fdcdaa817b
Twometer <twometer@outlook.de>
2018-08-11 16:06:46 +0200

    Added voting functionality

--- strawpoll-bot/Program.cs
+++ strawpoll-bot/Program.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using StrawpollBot.Choice;
 using StrawpollBot.CLI;
+using StrawpollBot.Proxy;
+using StrawpollBot.Voting;
 
 namespace StrawpollBot
 {
@@ -27,7 +33,41 @@ namespace StrawpollBot
             Console.WriteLine("Choice Index: " + input.ChoiceIdx);
             Console.WriteLine("Vote count: " + input.VoteCount);
             Console.WriteLine("Proxy list: " + (input.ProxyList != null ? input.ProxyList.Name : "Do not use"));
+            Console.WriteLine();
 
+            Console.WriteLine("Preparing...");
+            var choiceId = ChoiceFinder.FindChoiceId(input.PollId, input.ChoiceIdx);
+            if (choiceId == null)
+            {
+                Console.WriteLine("Error: There is no choice with index " + input.ChoiceIdx);
+                return;
+            }
+            Console.WriteLine("Choice id: " + choiceId);
+
+            var proxyList = ProxyListParser.ParseProxyList(input.ProxyList);
+            Console.WriteLine("Executing votes...");
+
+            var executor = new VoteExecutor(input.PollId, choiceId);
+            var tasks = new List<Task>();
+            var doneVotes = 0;
+
+            for (var i = 0; i < input.VoteCount; i++)
+            {
+                tasks.Add(Task.Run(() =>
+                {
+                    while (true)
+                    {
+                        if (executor.Vote(proxyList.NextProxy()))
+                        {
+                            Interlocked.Increment(ref doneVotes);
+                            Console.WriteLine($"Finished vote {doneVotes}/{input.VoteCount}");
+                            break;
+                        }
+                    }
+                }));
+            }
+
+            Task.WaitAll(tasks.ToArray(), -1);
         }
     }
 }
